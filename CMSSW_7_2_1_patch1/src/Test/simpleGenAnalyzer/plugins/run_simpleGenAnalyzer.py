595ec61574f50f7544834350c66ee1b0a2c38994
Ian Laflotte <ilaflott@lxplus0005.cern.ch>
2015-02-11 20:16:57 +0100

    i hate git

---
+++ CMSSW_7_2_1_patch1/src/Test/simpleGenAnalyzer/plugins/run_simpleGenAnalyzer.py
@@ -0,0 +1,28 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring("/Neutrino_Pt-2to20_gun/Fall13dr-castor_tsg_PU1bx50_POSTLS162_V1-v1/AODSIM")    
+    #fileNames = cms.untracked.vstring("file:/afs/cern.ch/work/i/ilaflott/public/test_files/AODSIM_TEST_FILE.root")    
+    #fileNames = cms.untracked.vstring("root://cms-xrd-global.cern.ch//store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_POSTLS170_V5-v1/00000/F6EDDC10-8DFC-E311-BC5D-0025905A60D6.root")
+    
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("testing.root"),
+    closeFileFast = cms.untracked.bool(True)
+)
+
+
+
+process.demo = cms.EDAnalyzer('simpleGenAnalyzer',
+    gen = cms.InputTag("genParticles")
+)
+
+
+process.p = cms.Path(process.demo)
